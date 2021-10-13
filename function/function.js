4045c729366d7bb73ae3eafe4265c03e2b514775
Kolya Samchuk <mykola.samchuk@gmail.com>
2021-10-13 20:26:58 +0300

    add function deleteLetters in function block

--- function/function.js
+++ function/function.js
@@ -8,10 +8,11 @@ function getMaxDigit(number){
         if(arrNumber[i] > max){
             max = arrNumber[i]
         }
-    }
-    console.log(max)
+    };
+    
+    return max
 }
- getMaxDigit(1236)
+ console.log(getMaxDigit(1236))
 
 // Створити функцію, яка форматує ім'я, роблячи першу букву великою. 
 // ("влад" -> "Влад", "вЛАД" -> "Влад" і так далі);
@@ -24,6 +25,7 @@ function getFirstLetterUpper(word){
     return wordUpper;
 }
 console.log(getFirstLetterUpper('AbraCadabra'))
+
 // Створити функцію, яка рахує скільки разів певна буква повторюється в слові.
 // Приклад: countLetter("а", "Асталавіста") -> 4
 function countLetter(letter, word){
@@ -31,12 +33,28 @@ function countLetter(letter, word){
     const wordLowerArr = String(word).toLowerCase().split(``);
     let totlaLetter = 0;
 
-    for(let i= 0; i<=wordLowerArr.length; i++){
+    for(let i= 0; i < wordLowerArr.length; i++){
         if(wordLowerArr[i] == letterLower){
             totlaLetter = totlaLetter + 1 
         }
-    }
-    return totlaLetter
-    
+    };
+
+    return totlaLetter;
 }
 console.log(countLetter(`a`, `Astalavista`))
+
+// Створіть функцію, яка видаляє всі букви з речення. 
+// Приклад: deleteLetters('a', "blablabla") -> "blblbl"
+function deleteLetters(letter, word){
+    const letterLower = String(letter).toLowerCase();
+    const wordLowerArr = String(word).toLowerCase().split(``);
+    let arrWord = []
+    for(let i = 0; i < wordLowerArr.length; i++){
+        if(letterLower !== wordLowerArr[i]){
+            arrWord.push(wordLowerArr[i])
+        }
+    }
+     
+    return arrWord.join("")
+}
+console.log(deleteLetters(`a`, `blablabla`))
